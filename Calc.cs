7e55f2e89f6cf1e98e56598aa7939fe48421c34f
grubertmayor <vstarovoyt29@gmail.com>
2021-05-16 20:19:24 +0300

    add class Calc

---
+++ Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectStar
+{
+    class Calc
+    {
+        public string Hello(string name)
+        {
+            return "Hello " + name;
+        }
+        public double Add(double arg0,double arg1)
+        {
+            return arg0 + arg1;
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 
 namespace UnitTestProjectStar
 {
     [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        //[TestMethod]
+        Calc calc = new Calc();
+        [Test]
+        public void TestMethodAdd()
         {
+            
+            double expected = 123;
+            double actual;
+
+            actual = calc.Add(120, 3);
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0.0001);
+        }
+        [Test]
+        public void TestMethodHello()
+        {
+            
+            string expected = "Hello Abu";
+            string actual;
+
+            actual = calc.Hello("Abu");
+            NUnit.Framework.Assert.AreEqual(expected, actual);
+
         }
     }
 }
